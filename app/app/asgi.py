f1207fea0534a0a75690813b89a68878fec20ff9
Nico Felbinger <26925347+felbinger@users.noreply.github.com>
2022-06-12 16:18:28 +0200

    added event loop

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -6,7 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
-
+import asyncio
 import os
 
 from django.core.asgi import get_asgi_application
@@ -14,3 +14,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_asgi_application()
+
+loop = asyncio.get_event_loop()
